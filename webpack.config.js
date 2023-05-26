df5d80b72bb656aaee3399c4f0486849cbd36b77
Thalia <ecomdragona@gmail.com>
2023-05-26 16:30:07 -0700

    project-push

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: {
+		index: './src/index.js',
+		print: './src/print.js'
+	},
+	devtool: 'inline-source-map',
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Development',
+		}),
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	mode: 'development'
+};
